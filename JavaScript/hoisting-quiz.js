55fb5337294a4a55b63d832bdbdacc27ec1b8b7b
Babu Miya Mohammad <moba0222@netcracker.com>
2023-01-09 14:59:48 +0530

    js - classes-es6

--- JavaScript/hoisting-quiz.js
+++ JavaScript/hoisting-quiz.js
@@ -13,11 +13,14 @@ function fn(){
 function fn(){
     return 30;
 }
-let f = function() {
-    return true;
+function f(a,b) {
+    return 
+    {a + b};
 }
 // let f = function(){
 //     return false;
 // }
 console.log(fn());  // prints the latest function declaration return value 30;
-console.log(f()); 
+console.log(f(2,3)); 
+
+// --------------------------------------------------------------
