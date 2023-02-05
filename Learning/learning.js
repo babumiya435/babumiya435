5866c2fce2bf1f05ea918728e98364395e921060
Babu Miya Mohammad <moba0222@netcracker.com>
2023-02-05 19:23:57 +0530

    learning-code-one-way-data-binding and async-defer-html-parsing

--- Learning/learning.js
+++ Learning/learning.js
@@ -1,1 +1,21 @@
 console.log("Target 5");
+console.log("inside js file");
+var data = [];
+function createData(n) {
+    if(!n){
+        return null;
+    }
+    for (let i = 0; i < n; i++) {
+        let d = {
+            fName : "Sameer",
+            lName : "Mohammad",
+            age : 28
+        }
+        data.push(d);
+    }
+}
+createData(10);
+let ele = document.getElementById("data");
+ele.innerHTML = data[0].fName;
+console.log(data);
+console.log(ele);
