fc1015391fd93d4bd1bd4fa7f05bbc17e1bc4df3
Babu Miya Mohammad <moba0222@netcracker.com>
2023-02-10 15:46:57 +0530

    javascript-class2

---
+++ Learning/Interview-Prep/08-02-2023-interview/JS/heigherOrderFunction.js
@@ -0,0 +1,5 @@
+console.log("Heigher Order Functions in JS");
+// https://levelup.gitconnected.com/sneak-peak-of-map-filter-and-reduce-in-javascript-79d38181a48
+let users = [
+    {name : "student1"}
+    ]
