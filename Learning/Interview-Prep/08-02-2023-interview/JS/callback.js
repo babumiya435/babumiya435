0604ae5498eecb6482c9cca47f40bfd52150b816
Babu Miya Mohammad <moba0222@netcracker.com>
2023-02-13 22:30:21 +0530

    javascript-class11

--- Learning/Interview-Prep/08-02-2023-interview/JS/callback.js
+++ Learning/Interview-Prep/08-02-2023-interview/JS/callback.js
@@ -37,23 +37,23 @@ const mainFunction = (callback)=>{
 // button.addEventListener('click',callback);
 
 // synchronous call back example
-console.log("------------Synchronous CODE-----------");
-console.log("Start");
+// console.log("------------Synchronous CODE-----------");
+// console.log("Start");
 function getGreeting(name,cb){
     cb(name);
 }
-console.log("Before Greeting");
+// console.log("Before Greeting");
 function cb(cbName){
     console.log(`Hello ${cbName}`);
 }
-getGreeting("Sameer",cb);
-console.log("After Greeting");
-console.log("End");
-console.log("------------Synchronous CODE-----------");
+// getGreeting("Sameer",cb);
+// console.log("After Greeting");
+// console.log("End");
+// console.log("------------Synchronous CODE-----------");
 
 // Asynchronous call back example
-console.log("------------ASynchronous CODE-----------");
-console.log("Start");
+// console.log("------------ASynchronous CODE-----------");
+// console.log("Start");
 function getGreeting1(name,cb){
     setTimeout(()=>{
         cb(name,function (name){ console.log(`Hello ${name}`)});
@@ -69,35 +69,59 @@ function cb2(cbName){
 function cb3(cbName){
     console.log(`Hello ${cbName}`)
 }
-console.log("Before greeting");
-getGreeting1("Sameer",function (cbName,cb1){
-        console.log(`Hello ${cbName}`);
-        cb1(cbName);
-    }
-);
-console.log("After greeting");
-console.log("end");
-console.log("------------ASynchronous CODE-----------");
+// console.log("Before greeting");
+// getGreeting1("Sameer",function (cbName,cb1){
+//         console.log(`Hello ${cbName}`);
+//         cb1(cbName);
+//     }
+// );
+// console.log("After greeting");
+// console.log("end");
+// console.log("------------ASynchronous CODE-----------");
 
 function One(two){
     console.log("START");
     console.log("ONE")
+    //calling second function
     two(Three);
 }
 function Two(three){
     console.log("TWO");
+    //calling third function
     three(Four);
 }
 function Three(four){
     console.log("THREE");
+    //calling fourth function
     four(Five);
 }
 function Four(five){
     console.log("FOUR");
+    //calling fifth function
     five()
 }
 function Five(){
     console.log("FIVE");
     console.log("END")
 }
-One(Two);
+//calling first function
+// One(Two);
+// Possible solutions to call back hell
+// 1. Write comments
+// 2. Split functions into smaller functions --> functions One,Two,Three,Four,Five
+// 3. Using Promises 
+// Promise is an object which represents eventual completion/failure of asynchronous call.
+let promise = new Promise((res,rej)=>{
+    setTimeout(()=>{
+        rej("rejected");
+        res("resolved"); 
+    },1000)
+})
+promise.then((response)=>{
+    console.log("Resolved data : " + response);
+}).catch((error)=>{
+    console.log("Rejection Error : " + error)
+})
+console.log(promise);
+
+// 4. Using Async/await
