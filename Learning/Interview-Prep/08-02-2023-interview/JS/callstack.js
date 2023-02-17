224d0d3013864fdb337c6318d2ca997db5fc0072
Babu Miya Mohammad <moba0222@netcracker.com>
2023-02-17 20:17:10 +0530

    javascript-learning-20

---
+++ Learning/Interview-Prep/08-02-2023-interview/JS/callstack.js
@@ -0,0 +1,6 @@
+console.log("Call stack");
+// 1.Call stack - > main FILO thred or container responsible for executing all the executable statements or functions
+// 2.Heap/memory : responsible for memorry allocation for all the variables and functions and closures
+// 3.Web API : document,setTimeout,ajax()
+// 4.Call back Queue: Responsible for storing all the asynchronous calls/function in order of priority.
+// 5.Promise Queue: Responsible for keep track of promise functions which are in higher priority than call back queue.
