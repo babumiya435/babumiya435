d6f9c4d9484a325dc004e150c9bd8fa80f0a0e8b
Babu Miya Mohammad <moba0222@netcracker.com>
2023-02-20 16:15:51 +0530

    javascript-learning-29

--- Learning/Interview-Prep/08-02-2023-interview/JS/async-await.js
+++ Learning/Interview-Prep/08-02-2023-interview/JS/async-await.js
@@ -1,7 +1,8 @@
 console.log("---------Async-Await-----------")
 const promise1 = new Promise((res,rej)=>{
     setTimeout(()=>{
-        res("promise1")
+        // res("promise1")
+        rej("ERROR-promise1")
     },10000)
 })
 const promise2 = new Promise((res,rej)=>{
@@ -11,19 +12,31 @@ const promise2 = new Promise((res,rej)=>{
 })
 function normalRun(){
     let x = promise1.then((x)=>{
-        console.log('normal promise',x);
+        console.log('normal promise-x',x);
+    }).catch(()=>{
+        console.log("error-normal");
     })
     let y = promise2.then((x)=>{
-        console.log('normal promise',x);
+        console.log('normal promise-y',x);
     })
     // console.log(x);
     // console.log(y);
 }
-normalRun();
+// normalRun();
 async function run(){
-    let x = await promise1;
-    let y = await promise2;
-    console.log('async-await',x);
-    console.log('async-await',y);
+    // try{
+    //     let x = await promise1;
+    //     let y = await promise2;
+    //     console.log('async-await -x',x);
+    //     console.log('async-await- y',y);
+    // }catch(error){
+    //     console.log(error);
+    // }
+    const [ item, error ] = await promise2;
+    if (error) {  // handle error and return
+        console.log(null);
+        } else {
+            console.log(item);
+        }  
 }
 run();
